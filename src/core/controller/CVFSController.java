a615acc83ee75d854d360bd0af796f867f0b4223
MatthewRiley05 <matthewrileyraymundo@gmail.com>
2024-11-20 15:48:09 +0800

    fix: ensure proper exit sequence in CVFSController

--- src/core/controller/CVFSController.java
+++ src/core/controller/CVFSController.java
@@ -220,9 +220,9 @@ public class CVFSController {
                         cvfs.save(filePath);
                     }
                 }
-                    System.out.println("Exiting CVFS...");
-                    System.exit(0);
-
+                System.out.println("Exiting CVFS...");
+                System.exit(0);
+                break;
         }
     }
 }
